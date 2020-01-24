33de856ff75c20d8f3f87ead70edfd95ca5ae959
Mafrans <malte.kluft@gmail.com>
2020-01-24 10:45:31 +0100

    Prepare for input system

--- src/main/java/me/mafrans/hangman/Hangman.java
+++ src/main/java/me/mafrans/hangman/Hangman.java
@@ -3,7 +3,7 @@ package me.mafrans.hangman;
 public class Hangman {
     public static void main(String[] args) {
         Wordgenerator wordgenerator = new Wordgenerator();
-        HangmanRenderer renderer = new HangmanRenderer(wordgenerator.generate());
+        HangmanManager renderer = new HangmanManager(wordgenerator.generate());
         renderer.render(5);
     }
 }
--- src/main/java/me/mafrans/hangman/HangmanManager.java
+++ src/main/java/me/mafrans/hangman/HangmanManager.java
@@ -1,13 +1,18 @@
 package me.mafrans.hangman;
 
+import org.apache.commons.lang.StringUtils;
+
 import java.awt.*;
+import java.beans.Expression;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class HangmanRenderer {
+public class HangmanManager {
     private Letter[] letters;
-    private boolean[] visible;
+    private int failures;
 
-    public HangmanRenderer(String word) {
+    public HangmanManager(String word) {
         letters = new Letter[word.length()];
         for (int i = 0; i < word.length(); i++) { // Loop over every letter in the word
             String ch = String.valueOf(word.charAt(i));
@@ -18,12 +23,22 @@ public class HangmanRenderer {
         }
     }
 
-    public void showLetter(String letter) {
+    public void tryLetter(String letter) {
+        if (!showLetter(letter)) {
+            failures++;
+        }
+        render(failures);
+    }
+
+    public boolean showLetter(String letter) {
+        boolean out = false;
         for(Letter l : letters) {
             if (l.letter.equalsIgnoreCase(letter)) {
                 l.visible = true;
+                out = true;
             }
         }
+        return out;
     }
 
     public void render(int failures) {
@@ -32,6 +47,14 @@ public class HangmanRenderer {
         while(in.hasNextLine()) {
             bob.append(in.nextLine() + "\n");
         }
+        bob.append("\n");
+        bob.append(StringUtils.repeat(" ", (28 - letters.length*2)/2));
+
+        for(Letter l : letters) {
+            bob.append((l.visible || l.letter.equals(" ") ? l.letter : "_") + " ");
+        }
+        bob.append("\n");
+
         Console.render(bob.toString());
     }
 }
