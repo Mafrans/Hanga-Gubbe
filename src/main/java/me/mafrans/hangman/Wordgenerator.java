57d1df8222aa72e6f975b60ba8af14b88a1e4a68
TheDeniableToast <filipnorberg02@outlook.com>
2020-01-30 10:33:47 +0100

    comments started

--- src/main/java/me/mafrans/hangman/Wordgenerator.java
+++ src/main/java/me/mafrans/hangman/Wordgenerator.java
@@ -8,6 +8,7 @@ import java.util.Set;
 public class Wordgenerator
 {
     private String[] wordlist;
+    
     public Wordgenerator()
     {
         Scanner scanner = new Scanner(ClassLoader.getSystemResourceAsStream("wordlist.csv"));
